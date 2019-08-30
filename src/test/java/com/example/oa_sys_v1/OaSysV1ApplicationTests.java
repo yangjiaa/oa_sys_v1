8bc59285befa97713eb8b64a717f32c4a419a544
Administrator <302831892@qq.com>
2019-08-30 19:35:09 +0800

    Mod pom修改

--- src/test/java/com/example/oa_sys_v1/OaSysV1ApplicationTests.java
+++ src/test/java/com/example/oa_sys_v1/OaSysV1ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.oa_sys_v1;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OaSysV1ApplicationTests {
 
-	@Test
+	@Ignore
 	void contextLoads() {
+		System.out.println("1234");
 	}
-
+	@Test
+	void contextLoad() {
+		System.out.println("4321");
+	}
 }
